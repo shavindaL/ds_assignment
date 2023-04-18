758b10d0cae5643e62e326a616a57fa731b39e69
shavindaL <shavinda724@gmail.com>
2023-04-19 04:33:15 +0530

    add 'Add to Cart' feature

--- frontend/components/Shoppingcart.jsx
+++ frontend/components/Shoppingcart.jsx
@@ -1,111 +1,130 @@
-import styles from '../styles/Shoppingcart.module.css';
-import React, { useState } from 'react';
+import styles from "../styles/Shoppingcart.module.css";
+import React, { useEffect, useState } from "react";
 
 function Shopcart() {
-  const [products, setProducts] = useState([
-    { id: 1, name: 'Lorem ipsum dolor sit', description: 'Lorem ipsum dolor sit', price: 12.99, quantity: 2 },
-    { id: 2, name: 'Sorem ipsum dolor sit', description: 'Lorem ipsum dolor sit', price: 45.99, quantity: 1 }
-  ]);
+  const [cartItems, setCartItems] = useState([]);
+  const [isEmpty, setIsEmpty] = useState(false);
+  let grandTotal = 0;
 
-  const [discountRate] = useState(0.05);
-  const [shippingRate] = useState(15.00);
+  let cs;
 
-  function updateQuantity(id, newQuantity) {
-    const newProducts = products.map((product) => {
-      if (product.id === id) {
-        return { ...product, quantity: newQuantity };
-      }
-      return product;
+  useEffect(() => {
+    cs = localStorage.getItem("cart");
+
+    if (!cs) {
+      setIsEmpty(true);
+      return;
+    }
+    const cart = JSON.parse(cs);
+    setCartItems(cart.products);
+  }, []);
+
+  const removeItem = (productId) => {
+    cartItems.map((cartItem) =>
+      grandTotal += cartItem.price * cartItem.qty
+    )
+    const result = cartItems.filter((cartItem) => {
+      return cartItem.id !== productId;
     });
-    setProducts(newProducts);
-  }
+    setCartItems(result);
+  };
 
-  function removeItem(id) {
-    const newProducts = products.filter((product) => product.id !== id);
-    setProducts(newProducts);
-  }
-
-  function recalculateCart() {
-    const subtotal = products.reduce((acc, product) => acc + (product.price * product.quantity), 0);
-    const discount = subtotal * discountRate;
-    const shipping = subtotal > 0 ? shippingRate : 0;
-    const total = subtotal - discount + shipping;
-
-    return (
-      <>
-        <div className={styles.totalsItem1}>
-          <label>Subtotal:</label>
-          <div className="totals-value" id={styles.cartSubtotal}>{subtotal.toFixed(2)}</div>
-        </div>
-        <div className={styles.totalsItem2}>
-          <label>Discount(5%):</label>
-          <div className="totals-value" id={styles.cartDiscount}>{discount.toFixed(2)}</div>
-        </div>
-        <div className={styles.totalsItem3}>
-          <label>Delivery:</label>
-          <div className="totals-value" id={styles.cartShipping}>{shipping.toFixed(2)}</div>
-        </div>
-        <div className={styles.totalsItem4}>
-          <label>Grand Total:</label>
-          <div className="totals-value" id={styles.cartTotal}>{total.toFixed(2)}</div>
-        </div>
-      </>
-    );
-  }
-    return ( 
-        <>
-        
-            <h1>Shopping Cart</h1>
-
-            <div class={styles.shoppingCart}>
-
-                <div class={styles.columnLabels}>
-                    <label class="product-image">Image</label>
-                    <label class="product-details">Product</label>
-                    <label class="product-price1">Price</label>
-                    <label class="product-quantity1">Quantity</label>
-                    <label class="product-removal1">Remove</label>
-                    <label class="product-line-price1">Total</label>
-                </div>
-
-                {products.map((product) => (
-
-                    <div className="product" key={product.id}>
-                        <div className={styles.productImage}>
-                            <img src={`img${product.id}.png`} alt={`${product.name} image`} />
-                        </div>
-                        <div className="product-details">
-                            <div className={styles.productTitle}>{product.name}</div>
-                            <p className={styles.productDescription}>{product.description}</p>
-                        </div>
-                        <div className={styles.productPrice}>{product.price.toFixed(2)}</div>
-                        <div className={styles.productQuantity}>
-                            <input
-                                type="number"
-                                value={product.quantity}
-                                onChange={(event) => updateQuantity(product.id, parseInt(event.target.value))}
-                                min="1"
+  return (
+    <>
+      <div class="flex flex-col w-80vw mx-auto">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+              <table class="min-w-full text-center text-lg font-light">
+                <thead class="border-b bg-green-6 font-roboto text-white h-20">
+                  <tr>
+                    <th scope="col" class=" px-6 py-4"></th>
+                    <th scope="col" class=" px-6 py-4">
+                      Description
+                    </th>
+                    <th scope="col" class=" px-6 py-4">
+                      Quantity
+                    </th>
+                    <th scope="col" class=" px-6 py-4">
+                      Remove
+                    </th>
+                    <th scope="col" class=" px-6 py-4">
+                      Price
+                    </th>
+                    <th scope="col" class=" px-6 py-4">
+                      Total
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="font-roboto text-2xl font-500">
+                  {cartItems &&
+                    cartItems.map((cartItem) => {
+                      grandTotal += cartItem.price * cartItem.qty
+                      return (
+                        <tr
+                          class="border-b dark:border-neutral-500"
+                          key={cartItem.id}
+                        >
+                          <td class="whitespace-nowrap  px-6 py-4 font-medium">
+                            <img
+                              href={cartItem.imgLink}
+                              width={100}
+                              height={100}
                             />
-                        </div>
-                            <div className={styles.productRemoval}>
-                                <button onClick={() => removeItem(product.id)}>Remove</button>
-                            </div>
-                            <div className={styles.productLinePrice}>{(product.price * product.quantity).toFixed(2)}</div>
-                    </div>
-                ))}
-
-                <div className="totals">
-                    {recalculateCart()}
-                </div>
-
-               <div className={styles.checkout}>
-                    <button class={styles.checkout}>Checkout</button>
-                </div>
-
+                          </td>
+                          <td class="whitespace-nowrap  px-6 py-4 font-medium">
+                            {cartItem.description}
+                          </td>
+                          <td class="whitespace-nowrap  px-6 py-4 ">
+                            {cartItem.qty}
+                          </td>
+                          <td class="whitespace-nowrap  px-6 py-4">
+                            <button onClick={() => removeItem(cartItem.id)}>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width={20}
+                                fill="#F00"
+                                viewBox="0 0 448 512"
+                              >
+                                <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
+                              </svg>
+                            </button>
+                          </td>
+                          <td class="whitespace-nowrap  px-6 py-4">
+                            {cartItem.price}
+                          </td>
+                          <td class="whitespace-nowrap  px-6 py-4">
+                            {cartItem.price * cartItem.qty}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+                <tfoot>
+                  <tr className="h-16 text-2xl font-500">
+                    <td colSpan={5}>
+                      Grand Total
+                    </td>
+                    <td className="text-3xl font-500">
+                      {grandTotal}
+                    </td>
+                  </tr>
+                  <tr className="h-16">
+                    <td colSpan={5}></td>
+                    <td >
+                      <button className="text-white bg-green-7 border-solid border-2 px-5 py-2 rounded-lg hover:border-green-7 hover:text-green-7 hover:bg-white">
+                        Checkout
+                      </button>
+                    </td>
+                  </tr>
+                </tfoot>
+              </table>
             </div>
-           
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
- 
+
 export default Shopcart;
