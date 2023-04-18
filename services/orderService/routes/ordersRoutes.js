ec9ba951280f7f7d6410c0e4a1c9c053049ca5d6
Pasindu Kushan <https://github.com/it21110016>
2023-04-18 19:10:14 +0530

    updated orderService

--- services/orderService/routes/ordersRoutes.js
+++ services/orderService/routes/ordersRoutes.js
@@ -1,9 +1,32 @@
+// Import all the methods from orderController
+const {
+    getOrderService,
+    addOrder,
+    getOrder,
+    updateOrder,
+    deleteOrder
+} = require('../controller/orderController');
+
+// Import the express module
 const express = require("express");
+
+// Create new router object
 const router = express.Router();
 
-const {getOrder} = require('../controller/orderController');
+// Handle GET request at root
+router.get("/", getOrderService);
 
-router.get("/", getOrder);
+// Handle POST request at "addOrder" URI
+router.post("/addOrder", addOrder);
 
+// Handle GET request at "orders/:id" URI
+router.get("/orders/:id", getOrder);
 
-module.exports =router;
+// Handle PUT request at "orders/:id" URI
+router.put("/orders/:id", updateOrder);
+
+// Handle DELETE request at "orders/:id" URI
+router.delete("/orders/:id", deleteOrder);
+
+// Export the router object
+module.exports = router;
