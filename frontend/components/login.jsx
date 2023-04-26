23c308c73f020ac5b9d766f5e35fa8429901baba
Maharanhindage V.A.R <it21109126@my.sliit.lk>
2023-04-26 18:15:09 +0530

    Signup form fixed

--- frontend/components/login.jsx
+++ frontend/components/login.jsx
@@ -163,7 +163,7 @@ export default function Login() {
                 <div className="text-center lg:text-left">
                   <button
                     disabled={isLoading}
-                    type="Submit"
+                    type="submit"
                     className="inline-block rounded bg-green-6 px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-green-9 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
                   >
                     Login
